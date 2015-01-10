927ba35c25cab81e9e88efeec11bda0937673765
TJ2 <TJ2@DriverStation.br.int>
2015-01-10 11:11:41 -0500

    added a gyro

--- src/org/usfirst/frc/team88/robot/Wiring.java
+++ src/org/usfirst/frc/team88/robot/Wiring.java
@@ -4,4 +4,5 @@ public class Wiring {
     public static final int leftMotorController = 3;
     public static final int rightMotorController = 4;
     public static final int middleMotorController=5;
+    public static final int GYRO=0;
 }
--- src/org/usfirst/frc/team88/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team88/robot/subsystems/Drive.java
@@ -5,27 +5,31 @@ import org.usfirst.frc.team88.robot.commands.DriveWithControllerSimple;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.hal.CanTalonSRX;
+import edu.wpi.first.wpilibj.Gyro ;
 
 /**
  *
  */
 public class Drive extends Subsystem {
-    private final CanTalonSRX lTalon, rTalon, mTalon;
+    private final CanTalonSRX lTalon, rTalon, mTalon; 
+    private final Gyro gyro;
 
     public Drive() {
     	lTalon = new CanTalonSRX(Wiring.leftMotorController);
     	rTalon = new CanTalonSRX(Wiring.rightMotorController);
     	mTalon = new CanTalonSRX(Wiring.middleMotorController);
+    	gyro = new Gyro(Wiring.GYRO);
     }
     
     public void driveSimple(double left, double right, double middle) {        
         lTalon.Set(left);
         rTalon.Set(right);
-        mTalon.Set(middle);
+        mTalon.Set(middle);        
         
         System.out.println("Left: " + left);
         System.out.println("Right: " + right);
         System.out.println("Middle: "+ middle);
+        System.out.println(gyro.getAngle()+ " degrees");
     }
     
     public void initDefaultCommand() {
