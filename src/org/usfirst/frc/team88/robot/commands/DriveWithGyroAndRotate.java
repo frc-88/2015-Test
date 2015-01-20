ba056ae10ef3bb703de47eb5df53798128c9921a
Nick Avtges <navtges@meditech.com>
2015-01-20 16:17:47 -0500

    Add command to drive with gyro and rotate

---
+++ src/org/usfirst/frc/team88/robot/commands/DriveWithGyroAndRotate.java
@@ -0,0 +1,64 @@
+package org.usfirst.frc.team88.robot.commands;
+
+import org.usfirst.frc.team88.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * This command will maintain the robot facing while allowing the driver to
+ * drive forward or sideways. We store the initial angle returned by the gyro
+ * when we start and adjust left and right power to compensate when the gyro
+ * reading drifts from that value.
+ */
+public class DriveWithGyroAndRotate extends Command {
+
+	private static final double MAX_FORWARD_POWER = 0.5;
+	private static final double FACING_THRESHOLD = 10.0;
+	private double desiredFacing;
+	
+    public DriveWithGyroAndRotate() {
+        requires(Robot.drive);
+    }
+
+    protected void initialize() {
+    	// store initial facing
+    	desiredFacing = Robot.drive.getFacing();
+    }
+
+    protected void execute() {
+    	double left, right, scale, facingAdjustment;
+        double forward = Robot.oi.getDriverLeftVerticalAxis() * MAX_FORWARD_POWER;
+        double sideways = Robot.oi.getDriverRightHorizontalAxis();
+        double rotation = Robot.oi.getDriverLeftZAxis() - Robot.oi.getDriverRightZAxis();
+        
+        desiredFacing += rotation * FACING_THRESHOLD;
+        facingAdjustment = Math.max(Math.min((Robot.drive.getFacing() - desiredFacing) / FACING_THRESHOLD, 1.0), -1.0);
+        
+        left = forward + facingAdjustment;
+        right = forward - facingAdjustment;
+
+        // scale values of left and right so they are between -1.0 and 1.0
+        if ((Math.abs(left) > 1.0) || (Math.abs(right) > 1.0)) {
+        	if (Math.abs(left)>Math.abs(right)) {
+        		scale = 1.0 / Math.abs(left);
+        	} else {
+        		scale = 1.0 / Math.abs(right);
+        	}
+        	left *= scale;
+        	right *= scale;
+        }
+        
+        Robot.drive.driveSimple(left, right, sideways);
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+
+}
