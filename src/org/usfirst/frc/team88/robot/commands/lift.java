cb0b0a77154c8e00a2fd1648eaeec07b75f342ca
TJ2 <TJ2@DriverStation.br.int>
2015-01-29 18:57:40 -0500

    More stuff. grabber and lift commands

---
+++ src/org/usfirst/frc/team88/robot/commands/lift.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team88.robot.commands;
+
+import org.usfirst.frc.team88.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class lift extends Command {
+
+    public lift() {
+    	super("lift");
+    	requires(Robot.lift);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        double liftSpeed = Robot.oi.getOperatorRightZAxis() - Robot.oi.getOperatorLeftZAxis();
+        Robot.lift.moveLift(liftSpeed);
+        System.out.print("Lifter Speed= " + liftSpeed);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
