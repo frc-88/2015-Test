f286214babcebc88f34c2fdcd07f7f7f3724bda2
TJ2 <TJ2@DriverStation.br.int>
2015-02-13 22:19:31 -0500

    working auto!

---
+++ src/org/usfirst/frc/team88/robot/commands/AutoDrive.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team88.robot.commands;
+
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class AutoDrive extends CommandGroup {
+    
+    public  AutoDrive() {
+    	addSequential(new DriveStraight(2.5));
+    }
+}
